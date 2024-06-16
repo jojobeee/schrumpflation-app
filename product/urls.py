04bb475b3284cc17ba1ec95358b3cf519723c3d4
Julian Becker <beckerju@th-brandenburg.de>
2024-06-16 22:09:21 +0200

    add product models and set up product index template

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product/<int:item_id>/', views.item, name='item'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the product index.")
+    product_list = Product.objects.all()
+    context = {'product_list': product_list}
+    return render(request, 'product/index.html', context)
+
+def item(request, item_id):
+    return HttpResponse(f"You're looking at product {item_id}.")
