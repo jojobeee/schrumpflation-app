0e30cdea3dfd380734062619307b93c66087954d
Julian Becker <beckerju@th-brandenburg.de>
2024-06-17 12:20:15 +0200

    namespacing url

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+app_name = 'product'
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<int:product_id>/', views.productDetail, name='productDetail'),
