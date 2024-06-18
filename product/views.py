8eaf8570f552e2273245cb9afd799b3d750d7ed2
Julian Becker <beckerju@th-brandenburg.de>
2024-06-18 21:22:55 +0200

    search for products or brands

--- product/views.py
+++ product/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from .models import Product, Purchase
+from django.db.models import Q
 
-# Create your views here.
-def index(request):
-    product_list = Product.objects.all()
-    context = {'product_list': product_list}
-    return render(request, 'product/index.html', context)
 
-def productDetail(request, product_id):
+def product_detail(request, product_id):
     product = Product.objects.get(id=product_id)
     purchases = Purchase.objects.filter(product=product)
     context = {'product': product,'purchases': purchases,}
     return render(request, 'product/productDetail.html', context)
+
+def product_list(request):
+    search_query = request.GET.get('search', '')
+    if search_query:
+        product_list = Product.objects.filter(
+            Q(name__icontains=search_query) | 
+            Q(brand__name__icontains=search_query)
+        )
+    else:
+        product_list = []
+    return render(request, 'product/index.html', {'product_list': product_list})
