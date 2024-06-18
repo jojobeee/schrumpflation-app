27d277adc308693a2cb3aa62379e1a253a3252e6
Julian Becker <beckerju@th-brandenburg.de>
2024-06-18 22:36:19 +0200

    add brand filter

--- product/views.py
+++ product/views.py
@@ -11,16 +11,32 @@ def product_detail(request, product_id):
 
 def product_list(request):
     search_query = request.GET.get('search', '')
+    brand_filter = request.GET.get('brand', '')
+
     if search_query:
         product_list = Product.objects.filter(
             Q(name__icontains=search_query) | 
             Q(brand__name__icontains=search_query)
         )
     else:
-        product_list = Product.objects.all()  
+        product_list = Product.objects.all()
+
+    if brand_filter:
+        product_list = product_list.filter(brand__name__icontains=brand_filter)
 
     paginator = Paginator(product_list, 10)  # 10 products per page
     page_number = request.GET.get('page')
     products = paginator.get_page(page_number)
 
-    return render(request, 'product/index.html', {'products': products})
+    # Get a list of brands for the current search query
+    brands = Product.objects.filter(
+        Q(name__icontains=search_query) | 
+        Q(brand__name__icontains=search_query)
+    ).values_list('brand__name', flat=True).distinct()
+
+    return render(request, 'product/index.html', {
+        'products': products,
+        'brands': brands,
+        'selected_brand': brand_filter,
+        'search_query': search_query
+    })
