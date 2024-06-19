733995da51d45e575cf4c5ece0c21cd1629ba42e
Julian Becker <beckerju@th-brandenburg.de>
2024-06-19 16:03:59 +0200

    add dummy data to db

--- product/management/commands/populate_db.py
+++ product/management/commands/populate_db.py
@@ -17,7 +17,10 @@ class Command(BaseCommand):
             Supermarket(name='Edeka'),
             Supermarket(name='Rewe'),
             Supermarket(name='Aldi'),
-            Supermarket(name='Lidl')
+            Supermarket(name='Lidl'),
+            Supermarket(name='Kaufland'),
+            Supermarket(name='DM'),
+            Supermarket(name='Penny')
         ]
         Supermarket.objects.bulk_create(supermarkets)
         print("Supermarkets created")
@@ -26,6 +29,10 @@ class Command(BaseCommand):
             Brand(name='Ory'),
             Brand(name='Garnier'),
             Brand(name='Marabou'),
+            Brand(name='Pringles'),
+            Brand(name='Neutrogena'),
+            Brand(name='Naturgut'),
+            Brand(name="Gaudas's Glorie"),
         ]
         Brand.objects.bulk_create(brands)
         print("Brands created")
@@ -35,6 +42,10 @@ class Command(BaseCommand):
             Product(name='Fructis Shampoo', product_type="Shampoo", brand=Brand.objects.get(name='Garnier')),
             Product(name='Fructis Spülung', product_type="Spülung", brand=Brand.objects.get(name='Garnier')),
             Product(name='Mjölkchoklad', product_type="Schokolade", brand=Brand.objects.get(name='Marabou')),
+            Product(name='Pringles - versch. Sorten', product_type="Chips", brand=Brand.objects.get(name='Pringles')),
+            Product(name='Deep Moisture', product_type="Bodylotion", brand=Brand.objects.get(name='Neutrogena')),
+            Product(name='Babyspinat', product_type="Babyspinat", brand=Brand.objects.get(name='Naturgut')),
+            Product(name="Sweet Onion Sauce", product_type="Sauce", brand=Brand.objects.get(name="Gaudas's Glorie"))
         ]
         Product.objects.bulk_create(products)
         print("Products created")
@@ -55,7 +66,27 @@ class Command(BaseCommand):
             Purchase(product=Product.objects.get(name='Mjölkchoklad'), supermarket=Supermarket.objects.get(name='Edeka'),
                      size=250, unit='g', price=3.99, currency='EUR', purchase_date=date(2023, 1, 9)),
             Purchase(product=Product.objects.get(name='Mjölkchoklad'), supermarket=Supermarket.objects.get(name='Edeka'),
-                     size=220, unit='g', price=3.99, currency='EUR', purchase_date=date(2023, 6, 10)),   
+                     size=220, unit='g', price=3.99, currency='EUR', purchase_date=date(2023, 6, 10)),
+            Purchase(product=Product.objects.get(name='Pringles - versch. Sorten'), supermarket=Supermarket.objects.get(name='Kaufland'),
+                     size=200, unit='g', price=2.99, currency='EUR', purchase_date=date(2022, 10, 11)), 
+            Purchase(product=Product.objects.get(name='Pringles - versch. Sorten'), supermarket=Supermarket.objects.get(name='Kaufland'),
+                     size=185, unit='g', price=2.99, currency='EUR', purchase_date=date(2023, 3, 10)), 
+            Purchase(product=Product.objects.get(name='Pringles - versch. Sorten'), supermarket=Supermarket.objects.get(name='Kaufland'),
+                     size=165, unit='g', price=2.99, currency='EUR', purchase_date=date(2024, 6, 8)),
+            Purchase(product=Product.objects.get(name='Deep Moisture'), supermarket=Supermarket.objects.get(name='DM'),
+                     size=400, unit='ml', price=4.95, currency='EUR', purchase_date=date(2023, 11, 18)),
+            Purchase(product=Product.objects.get(name='Deep Moisture'), supermarket=Supermarket.objects.get(name='DM'),
+                     size=250, unit='ml', price=3.95, currency='EUR', purchase_date=date(2024, 5, 28)),
+            Purchase(product=Product.objects.get(name='Babyspinat'), supermarket=Supermarket.objects.get(name='Penny'),
+                     size=150, unit='g', price=1.89, currency='EUR', purchase_date=date(2022, 4, 7)),
+            Purchase(product=Product.objects.get(name='Babyspinat'), supermarket=Supermarket.objects.get(name='Penny'),
+                     size=125, unit='g', price=1.89, currency='EUR', purchase_date=date(2023, 5, 14)),
+            Purchase(product=Product.objects.get(name='Babyspinat'), supermarket=Supermarket.objects.get(name='Penny'),
+                     size=100, unit='g', price=1.89, currency='EUR', purchase_date=date(2024, 6, 15)),
+            Purchase(product=Product.objects.get(name='Sweet Onion Sauce'), supermarket=Supermarket.objects.get(name='Rewe'),
+                     size=650, unit='ml', price=3.99, currency='EUR', purchase_date=date(2022, 7, 25)),
+            Purchase(product=Product.objects.get(name='Sweet Onion Sauce'), supermarket=Supermarket.objects.get(name='Rewe'),
+                     size=550, unit='ml', price=3.99, currency='EUR', purchase_date=date(2024, 4, 5)),
         ]
         Purchase.objects.bulk_create(purchases)
         print("Purchases created")
