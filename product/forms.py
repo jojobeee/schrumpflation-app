7d21c4700a3fc10d65add3c8f8be1eac336d81c3
Julian Becker <beckerju@th-brandenburg.de>
2024-06-21 17:33:08 +0200

    validation for supermarket and brand in add purchase form

--- product/forms.py
+++ product/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Product, Purchase, Brand, Supermarket
+from django.core.exceptions import ValidationError
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -23,7 +25,7 @@ class SupermarketForm(forms.ModelForm):
 
 class CombinedAddSchrumpflationForm(forms.Form):
     product_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'id': 'product-name'}))
-    new_product_type = forms.CharField(max_length=100, required=False)
+    new_product_type = forms.CharField(max_length=100, required=True)
     existing_brand = forms.ModelChoiceField(queryset=Brand.objects.all().order_by('name'), required=False, empty_label="Marke auswählen")
     new_brand = forms.CharField(max_length=100, required=False)
     existing_supermarket = forms.ModelChoiceField(queryset=Supermarket.objects.all().order_by('name'), required=False, empty_label="Supermarkt auswählen")
@@ -33,3 +35,18 @@ class CombinedAddSchrumpflationForm(forms.Form):
     price = forms.DecimalField(max_digits=10, decimal_places=2)
     currency = forms.ChoiceField(choices=Purchase.CURRENCY_CHOICES, initial='EUR')
     purchase_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        existing_brand = cleaned_data.get('existing_brand')
+        new_brand = cleaned_data.get('new_brand')
+        existing_supermarket = cleaned_data.get('existing_supermarket')
+        new_supermarket = cleaned_data.get('new_supermarket')
+
+        if not existing_brand and not new_brand:
+            raise ValidationError('Bitte füllen Sie mindestens eins der beiden Felder "Marke" oder "oder neue Marke hinzufügen" aus!')
+
+        if not existing_supermarket and not new_supermarket:
+            raise ValidationError('Bitte füllen Sie mindestens eins der beiden Felder "Supermarkt" oder "oder neuen Supermarkt hinzufügen" aus!')
+
+        return cleaned_data
